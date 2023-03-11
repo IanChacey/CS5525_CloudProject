a292ce816293181ddf671b7ea971ccb474fd873b
Ian Chacey <basebal360@gmail.com>
2023-03-11 16:46:10 -0600

    revised shift list
    Now only shows the shifts for the current user, and also new shifts created use the employee id generated during account creation

---
+++ Models/ShiftIndexViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimeKeepingApp.Models
+{
+    public class ShiftIndexViewModel
+    {
+        public List<Shift> sList { get; set; }
+        public List<Employee> employeeList { get; set; }
+        public DateTime fromDate { get; set; }
+        public DateTime toDate { get; set; }
+        //public string descSearch { get; set; }
+        //public string debug { get; set; }
+        //public int pageNumber { get; set; }
+        //public string account { get; set; }
+
+        public int pageSize { get; set; } = 10;
+
+        public ShiftIndexViewModel(List<Shift> shifts,  List<Employee> employees = null)//DateTime start, DateTime end, int page = 1, string acct = "all")
+        {
+            sList = shifts;
+            employeeList = employees;
+            //descSearch = desc;
+            //fromDate = start;
+            //toDate = end;
+            //pageNumber = page;
+            //account = acct;
+
+        }
+    }
+}
