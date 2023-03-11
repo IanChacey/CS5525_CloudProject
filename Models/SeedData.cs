dcf121c6d77e51b0ed0ec1076bfeb2dc89fbe83b
Ian Chacey <basebal360@gmail.com>
2023-03-11 15:03:42 -0600

    initial commit

---
+++ Models/SeedData.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using TimeKeepingApp.Data;
+
+namespace TimeKeepingApp.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new ApplicationDbContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<ApplicationDbContext>>()))
+            {
+                if (context.Shift.Any() && context.Employee.Any())
+                {
+                    return;   // DB has been seeded
+                }
+                else if (!context.Shift.Any())
+                {
+                    context.Shift.AddRange(
+                        new Shift
+                        {
+                            EmployeeID = 0001,
+                            ShiftStart = DateTime.Parse("2023-2-12 12:00"),
+                            ShiftEnd = DateTime.Parse("2023-2-12 16:00"),
+                            Location = "On Site"
+                        },
+                        new Shift
+                        {
+                            EmployeeID = 0002,
+                            ShiftStart = DateTime.Parse("2023-2-12 08:00"),
+                            ShiftEnd = DateTime.Parse("2023-2-12 12:00"),
+                            Location = "On Site"
+                        }
+
+                    );
+                }
+                else if (!context.Employee.Any())
+                {
+                    context.Employee.AddRange(
+                        new Employee
+                        {
+                            EmployeeID = 0001,
+                            Department = "Managment",
+                            Role = "Manager",
+                            EmployeeName = "David",
+                            HourlyWage = 16.43f
+                        },
+                        new Employee
+                        {
+                            EmployeeID = 0002,
+                            Department = "Retail",
+                            Role = "Salesman",
+                            EmployeeName = "Kyle",
+                            HourlyWage = 12.74f
+                        }
+                    );
+                }
+                
+
+                
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Models/Shift.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TimeKeepingApp.Models
+{
+    public class Shift
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Employee ID Number")]
+        public int EmployeeID { get; set; }
+
+        [Display(Name = "Shift Starting Time")]
+        public DateTime ShiftStart { get; set; }
+
+        [Display(Name = "Shift Ending Time")]
+        public DateTime ShiftEnd { get; set; }
+
+        public string Location { get; set; }
+
+    }
+}
