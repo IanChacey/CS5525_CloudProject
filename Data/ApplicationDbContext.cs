c1a3bb0de2f52e3986016f52fe31496a9cbb14e2
Ian Chacey <basebal360@gmail.com>
2023-03-11 15:03:42 -0600

    initial commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TimeKeepingApp.Models;
+
+namespace TimeKeepingApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<TimeKeepingApp.Models.Shift> Shift { get; set; }
+        public DbSet<TimeKeepingApp.Models.Employee> Employee { get; set; }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TimeKeepingApp.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Employee ID Number")]
+        public int EmployeeID { get; set; }
+
+        public string? Department { get; set; }
+
+        public string Role { get; set; }
+
+        [Display(Name = "Employee Name")]
+        public string EmployeeName { get; set; }
+
+        public float HourlyWage { get; set; }
+    }
+}
