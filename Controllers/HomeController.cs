6356937371995c941cce287f3157c4f96213b84c
Ian Chacey <ikcmrp@mail.umkc.edu>
2023-03-14 00:35:06 -0500

    Hiding Information from unauthenticated users, low level users

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -29,6 +29,11 @@ namespace TimeKeepingApp.Controllers
             return View();
         }
 
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
