bef5d09ca56692df3b69ccb5d1daa64085b75aaa
Ian Chacey <ikcmrp@mail.umkc.edu>
2023-03-13 22:29:49 -0500

    Updates to employee and restraints on shifts

--- Data/Migrations/20230314032303_last_name.Designer.cs
+++ Data/Migrations/20230314032303_last_name.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TimeKeepingApp.Data;
 
 namespace TimeKeepingApp.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230314032303_last_name")]
+    partial class last_name
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
