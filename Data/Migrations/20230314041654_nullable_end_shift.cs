0c48633dcf3da64a42ddad011578e10178f7a893
Ian Chacey <ikcmrp@mail.umkc.edu>
2023-03-14 00:12:02 -0500

    Added ability to clock in and out

---
+++ Data/Migrations/20230314041654_nullable_end_shift.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TimeKeepingApp.Data.Migrations
+{
+    public partial class nullable_end_shift : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ShiftEnd",
+                table: "Shift",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EmployeeName",
+                table: "Employee",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EmployeeLastName",
+                table: "Employee",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ShiftEnd",
+                table: "Shift",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EmployeeName",
+                table: "Employee",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EmployeeLastName",
+                table: "Employee",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 20);
+        }
+    }
+}
